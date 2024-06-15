b958614f327c1d31f6352c8e44c3930d56ef7cb5
LucasBoe <lucas@lumotex.de>
2024-06-15 12:18:08 +0200

    improved character tween animator

--- scenes/character_tween_animator.gd
+++ scenes/character_tween_animator.gd
@@ -1,11 +1,7 @@
 extends Sprite2D
 
-enum AnimState { IDLE, WALK }
-
-var current_anim_state = AnimState.WALK
-var xOrientation = 1;
-
 @export var direction : Vector2 = Vector2(0,0);
+var xOrientation = 1;
 
 const SQUASH_STRENGTH = 0.05
 const IDLE_ANIMATION_SPEED = 3
@@ -13,38 +9,54 @@ const IDLE_PEAK_SHARPNESS = 4
 const WALK_ANIMATION_SPEED = 20.0
 const WALK_ROTATION_STRENGTH = .3
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var timeInSeconds = Time.get_ticks_msec() / 1000.0
-	var scaleOffset = sin(timeInSeconds * IDLE_ANIMATION_SPEED)
-	var scaleBase = pow(abs(scaleOffset), IDLE_PEAK_SHARPNESS) * SQUASH_STRENGTH
-	var xScale = 1 + abs(scaleBase - SQUASH_STRENGTH)
-	var yScale =  1 + scaleBase
 	
-	var rotationTarget = 0.0;
+	var timeInSeconds = Time.get_ticks_msec() / 1000.0
+	var target = idle_tween(timeInSeconds)
 	
 	if direction.length() > 0:
-		var t = timeInSeconds * WALK_ANIMATION_SPEED
-		var raw = pow(abs(sin(t)), .2) * sign(sin(t))
-		rotationTarget = raw * WALK_ROTATION_STRENGTH
+		target = walk_tween(timeInSeconds)
 		
-		var rawS = pow(abs(sin(t)), .4) * sign(sin(t))
+	var lerp = .2
+	
+	position = lerp(position, target.Position, lerp)
+	rotation = lerp(rotation, target.Rotation, lerp)
+	scale = lerp(scale, target.Scale, lerp)
+
+func walk_tween(timeInSeconds):
+	var t = timeInSeconds * WALK_ANIMATION_SPEED
+	var raw = pow(abs(sin(t)), .2) * sign(sin(t))
+	
+	if (direction.x):
+		xOrientation = sign(direction.x)
 		
-		scaleBase = abs(rawS) * SQUASH_STRENGTH
-		xScale =  1 + scaleBase
-		yScale = 1 + abs(scaleBase - SQUASH_STRENGTH)
+	var dirToRotation = Vector2(1, direction.y * 0.5).angle() * xOrientation
+	var rotationTarget = dirToRotation + raw * WALK_ROTATION_STRENGTH
 		
-		position = Vector2(0, 2 - abs(rawS) * 4)
+	var rawS = pow(abs(sin(t)), .4) * sign(sin(t))
+	var scaleBase = abs(rawS) * SQUASH_STRENGTH
+	
+	var x = (1 + scaleBase) * xOrientation
+	var y = 1 + abs(scaleBase - SQUASH_STRENGTH)
+	
+	var pos = Vector2(0, 2 - abs(rawS) * 4)
 		
-		if (direction.x):
-			xOrientation = sign(direction.x)
-	else:
-		position = Vector2.ZERO
-		
-	rotation = lerp(rotation, rotationTarget, .1)
-	scale = Vector2(xScale * xOrientation, yScale)
+	return TweenTargetData.new(pos ,rotationTarget ,Vector2(x, y))
+
+func idle_tween(timeInSeconds):
+	var scaleOffset = sin(timeInSeconds * IDLE_ANIMATION_SPEED)
+	var scaleBase = pow(abs(scaleOffset), IDLE_PEAK_SHARPNESS) * SQUASH_STRENGTH
+	var x = (1 + abs(scaleBase - SQUASH_STRENGTH)) * xOrientation
+	var y = 1 + scaleBase
+	return TweenTargetData.new(Vector2.ZERO, 0, Vector2(x, y))
+	
+class TweenTargetData:
+	@export var Position : Vector2
+	@export var Rotation : float
+	@export var Scale : Vector2
+	
+	func _init(position, rotation, scale):
+		Position = position
+		Rotation = rotation
+		Scale = scale
