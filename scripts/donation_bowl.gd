8be8e54b061479f6c1db40de0f03ff903891d588
Caroline Peter (home) <kerolinger@gmail.com>
2024-06-15 17:08:46 +0200

    donation bowl has a maximum limit of money

--- scripts/donation_bowl.gd
+++ scripts/donation_bowl.gd
@@ -5,6 +5,9 @@ var currentMoney = 15
 
 func _changeMoney(amount):
 	currentMoney += amount
+	
+	if currentMoney > donationLimit:
+		currentMoney = donationLimit
 
 func _on_interactable_zone_action_signal():
 
