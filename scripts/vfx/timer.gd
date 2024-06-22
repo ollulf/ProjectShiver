189c1337d93caae498ae56dc814daa7f596fbe7a
LucasBoe <lucas@lumotex.de>
2024-06-22 10:46:54 +0200

    fixes

--- scripts/vfx/timer.gd
+++ scripts/vfx/timer.gd
@@ -5,7 +5,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	$AudioStreamPlayer2D.play()
+	$Audio.play()
 	$AnimatedSprite2D.play()
 	create_tween().tween_callback(queue_free).set_delay(duration)
 
